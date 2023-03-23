691026eef5471a8b3c521c9c505a44c9040216f3
yangbofun <yangbofun@163.com>
2023-03-23 11:35:31 +0800

    replace gsed with sed

--- operators/speed_test/LongTail-Bench/long_tail_bench/script_for_cpu.sh
+++ operators/speed_test/LongTail-Bench/long_tail_bench/script_for_cpu.sh
@@ -2,10 +2,10 @@
 cp -R samples samples-bak
 dir=`dirname $0`/samples
 echo $dir
-gsed -i "s/.cuda()//g" `grep -rl ".cuda()"  $dir`
-gsed -i "s/device=\"cuda\",/device='cpu',/g" `grep -rl "device=\"cuda\","  $dir`
-gsed -i "s/device='cuda',/device='cpu',/g" `grep -rl "device='cuda',"  $dir`
+sed -i "s/.cuda()//g" `grep -rl ".cuda()"  $dir`
+sed -i "s/device=\"cuda\",/device='cpu',/g" `grep -rl "device=\"cuda\","  $dir`
+sed -i "s/device='cuda',/device='cpu',/g" `grep -rl "device='cuda',"  $dir`
 
-gsed -i "s/, device=\"cuda\"/, device='cpu'/g" `grep -rl ", device=\"cuda\""  $dir`
-gsed -i "s/, device='cuda'/, device='cpu'/g" `grep -rl ", device='cuda'"  $dir`
-gsed -i "s/,device='cuda'/, device='cpu'/g" `grep -rl ",device='cuda'"  $dir`
+sed -i "s/, device=\"cuda\"/, device='cpu'/g" `grep -rl ", device=\"cuda\""  $dir`
+sed -i "s/, device='cuda'/, device='cpu'/g" `grep -rl ", device='cuda'"  $dir`
+sed -i "s/,device='cuda'/, device='cpu'/g" `grep -rl ",device='cuda'"  $dir`
