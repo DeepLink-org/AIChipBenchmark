4ad825dbd03e59321c4e0dc1cc1bae130552a6fe
yangbofun <yangbofun@163.com>
2023-03-23 10:13:47 +0800

    add scripts for cpu test

---
+++ operators/speed_test/LongTail-Bench/long_tail_bench/script_for_cpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dir=`dirname $0`/samples
+echo $dir
+gsed -i "s/.cuda()//g" `grep -rl ".cuda()"  $dir`
+gsed -i "s/device=\"cuda\",/device='cpu',/g" `grep -rl "device=\"cuda\","  $dir`
+gsed -i "s/device='cuda',/device='cpu',/g" `grep -rl "device='cuda',"  $dir`
+
+gsed -i "s/, device=\"cuda\"/, device='cpu'/g" `grep -rl ", device=\"cuda\""  $dir`
+gsed -i "s/, device='cuda'/, device='cpu'/g" `grep -rl ", device='cuda'"  $dir`
+gsed -i "s/,device='cuda'/, device='cpu'/g" `grep -rl ",device='cuda'"  $dir`
